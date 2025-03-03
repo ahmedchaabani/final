9e76f602e99bfef65770be7be5af37bba5afd74a
useroussama <oussama.ouerghi@esprit.tn>
2025-03-03 12:32:25 +0100

    after change

--- src/Controller/laboController.php
+++ src/Controller/laboController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
